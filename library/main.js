f0111f32018079e3ff586891e6e3370dc6ae431a
Christopher Thomson <christopher.thomson42@gmail.com>
2017-07-11 14:28:50 -0700

    attempint to commit the better stuff

---
+++ library/main.js
@@ -0,0 +1,29 @@
+const shortid = require('shortid'); 
+shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
+
+class Dogs {
+    constructor() {
+        this.name,
+        this.breed
+    }
+    var dog1 = new Dogs
+        dog1.breed = 'a';
+        dog1.name = 'fido';
+    var dog2 = new Dogs
+        dog2.breed = 'b'
+        dog2.name = 'barky'
+
+    {
+        save(<objectToSave>)
+    }
+    {
+        get(id)
+    }
+    
+};
+
+
+
+module.exports = {
+
+};
