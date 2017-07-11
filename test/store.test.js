bd1a89139800a829e6ee7ee7b02cb163d9349e33
Anwar Montasir <anwarmontasir@gmail.com>
2017-07-11 13:47:08 -0700

    failing save test

--- test/store.test.js
+++ test/store.test.js
@@ -1,2 +1,11 @@
 const assert = require('assert');
-const store = require('../lib/store');
+const Store = require('../lib/class');
+const store = new Store();
+
+describe('save function', () => {
+    it('does save', () => {
+        const cat = { type: 'cat', name: 'felix' };
+        store.save(cat);
+        assert.equal(store.cat, { type: 'cat', name: 'felix' });
+    });
+});
