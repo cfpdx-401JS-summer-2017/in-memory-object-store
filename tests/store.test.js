e9c5e67280b3181c0c5f22a071eed322daee56eb
Mark Petrie <petrie.mark@gmail.com>
2017-07-11 14:52:14 -0700

    Initial working save method with id generation and passing mocha test

--- tests/store.test.js
+++ tests/store.test.js
@@ -1,8 +1,12 @@
 
 const assert = require('assert');
+const store = require('../lib/store');
 
-describe('memory store', () => {
-    it('works', () => {
-assert.equal(1+1, 42);
-    })
-})
+describe('in memory store', () => {
+    describe('save', () => {
+        it('generates an id', () => {
+            let savedObj = store.save({ foo: 'bar'});
+            assert.ok(savedObj._id);
+        });
+    });
+});
