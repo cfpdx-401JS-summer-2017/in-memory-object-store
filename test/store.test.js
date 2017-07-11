1fc7377a2984018b4be945c7677ae20a0451170a
Anwar Montasir <anwarmontasir@gmail.com>
2017-07-11 15:53:07 -0700

    passing tests for save and get

--- test/store.test.js
+++ test/store.test.js
@@ -2,10 +2,19 @@ const assert = require('assert');
 const store = require('../lib/store');
 
 describe('memory store', () => {
+    const cat = { type: 'cat', name: 'felix' };
+    const dog = { type: 'dog', name: 'fido' };
     describe('save', () => {
         it('generates an id', () => {
-            let savedObj = store.save({ foo: 'bar' });
-            assert.ok(savedObj._id);
+            let catObj = store.save(cat);
+            let dogObj = store.save(dog);
+            assert.ok(catObj._id);
         });
     });
+    describe('get', () => {
+        it('gets by id', () => {
+            let objectWithId = store.get(cat._id);
+            assert.ok(objectWithId);
+        });
+    })
 });
