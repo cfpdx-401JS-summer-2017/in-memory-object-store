3fca769026e5865a6b438972b4e1101c542f319b
Mark Petrie <petrie.mark@gmail.com>
2017-07-11 15:50:54 -0700

    Convert store.js to object-literal and add 2 more save method tests

--- tests/store.test.js
+++ tests/store.test.js
@@ -4,9 +4,19 @@ const store = require('../lib/store');
 
 describe('in memory store', () => {
     describe('save', () => {
-        it('generates an id', () => {
-            let savedObj = store.save({ foo: 'bar'});
+
+        let savedObj = store.save({ type: 'reef', name: 'banzai pipeline' });
+
+        it('generates and saves id', () => {
             assert.ok(savedObj._id);
+        });
+
+        it('saves with correct type property', () => {
+            assert.equal(savedObj.type, 'reef');
+        });
+
+        it('saves with correct name property', () => {
+            assert.equal(savedObj.name, 'banzai pipeline');
         });
     });
 });
