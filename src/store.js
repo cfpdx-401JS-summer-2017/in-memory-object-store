2a6a7a3d30822f690d6497ae274cfd656c694c5d
Haley Tortorella <hdtort@gmail.com>
2017-07-11 14:37:04 -0700

    passing save test

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const shortid = require('shortid');
+
+function save(obj) {
+    obj._id = shortid.generate()
+    return obj;
+}
+
+
+module.exports = { save: save };
---
+++ test/store.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const store = require('../src/store');
+
+describe('memory store', () => {
+    describe('save', () => {
+        it('generates an id', () => {
+            let savedObj = store.save({ foo: 'bar' });
+            assert.ok(savedObj._id);
+        });
+    });
+});
+
