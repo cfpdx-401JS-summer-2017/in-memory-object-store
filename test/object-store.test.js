d6a7cb452f43779410f90ed0197acd9954bf9193
Joe Churchman <churchman.joe@gmail.com>
2017-07-11 14:03:18 -0700

    add shortid npm package as runtime dependency, begin object store test

---
+++ test/object-store.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+var shortid = require('shortid');
+
+describe('object-store', () => {
+  it('contains appropriate methods and a place to store objects', () => {
+    const objectStore = 
+    assert.equal(objectStore, {});
+  });
+
+});
