9eddc49787af693778051decb0c2b89e9593218d
christylaguardia <christinelaguardia@gmail.com>
2017-07-11 14:47:25 -0700

    added save function to store.js

--- tests/store.test.js
+++ tests/store.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
+const store = require('../lib/store');
 
 describe('memory store', () => {
-    it('works', () => {
-        assert.equal(1+1, 2);
+    it('generates an id', () => {
+        let savedObj = store.save({ foo: 'bar' });
+        assert.ok(savedObj._id);
     });
 });
