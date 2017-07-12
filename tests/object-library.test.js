dec0a579c7403485356228cb52194831de3fa07f
Wendy Beck <webeck@gmail.com>
2017-07-12 00:34:48 -0700

    nothing much accomplished just tired

--- tests/object-library.test.js
+++ tests/object-library.test.js
@@ -3,12 +3,18 @@ const {nightOut} = require ('../lib/object-library');
 
 describe('object library', () => {
   it('creates an _id for an obj and returns it', () => {
-      let monday = new nightOut();
-      monday.save(monday);
-      return new Promise(function (resolve) {
-        // added a Promise because function was timing out
-        // it turned out to be because i hadn't required assert
-          resolve(assert.ok(monday._id));
-      })
-    });
+    let monday = new nightOut();
+    monday.save(monday);
+    let tuesday = new nightOut();
+    tuesday.save(tuesday);
+    return new Promise(function (resolve) {
+        resolve(assert.ok(monday._id) && assert.ok(tuesday._id));
+      });
   });
+  it('returns an object by id', () => {
+
+    return new Promise(function (resolve) {
+        resolve();
+      })
+  });
+});
