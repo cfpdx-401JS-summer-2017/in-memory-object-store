5491fe8e5ccddeba91132cf5d783e22eff297b5c
Christopher Thomson <christopher.thomson42@gmail.com>
2017-07-11 17:18:44 -0700

    added chai

--- test/test.js
+++ test/test.js
@@ -1,24 +1,26 @@
 const Store = require('../library/main');
+const chai = require('chai');
 const assert = require('assert');
+var assert = chai.assert;
 
 // describe('memory store', () => {
     
 // })
+
 describe('save', () => {
     it('creates an `_id` property on the object', () => {
         let store = new Store();
         let savedObj = store.save({name : 'fido'});
+        console.log(store.list);
         
-        
-        assert.ok(savedObj._id);
+        assert.property(savedObj, _id);
         
     });
     it('returns `objectToSave` with added `_id` property', () => {
         let store = new Store()
         let savedObj = store.save({name : 'fido'}); 
-        console.log(store);
 
-        assert.property('fido', _id);
+        assert.ok(savedObj._id);
 
     });
 });
@@ -28,8 +30,8 @@ describe('get', ()=> {
         let dog = {name : 'fido'};
         let savedObj = store.save(dog);
         let gettingIdObj = store.get(savedObj._id);
-        console.log(savedObj);
-        console.log(gettingId);
+        // console.log(savedObj);
+        // console.log(gettingId);
         assert.deepEqual(dog._id,gettingId);
 
     });
