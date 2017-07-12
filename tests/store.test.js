b5c61ba218177924f1021d17adac65411e56d4db
Mark Petrie <petrie.mark@gmail.com>
2017-07-11 20:44:16 -0700

    Force staging of ../tests/store.test.js and commit

--- tests/store.test.js
+++ tests/store.test.js
@@ -2,21 +2,40 @@
 const assert = require('assert');
 const store = require('../lib/store');
 
+const reef = { type: 'reef', name: 'Banzai Pipeline' };
+const point = { type: 'point', name: 'Barra de la Cruz' };
+const beach = { type: 'beach', name: 'Cloudbreak' };
+
+let savedObj1 = store.save(reef);
+let savedObj2 = store.save(point);
+let savedObj3 = store.save(beach);
+
 describe('in memory store', () => {
     describe('save', () => {
 
-        let savedObj = store.save({ type: 'reef', name: 'banzai pipeline' });
-
         it('generates and saves id', () => {
-            assert.ok(savedObj._id);
+            assert.ok(savedObj1._id);
         });
 
         it('saves with correct type property', () => {
-            assert.equal(savedObj.type, 'reef');
+            assert.equal(savedObj2.type, 'point');
         });
 
         it('saves with correct name property', () => {
-            assert.equal(savedObj.name, 'banzai pipeline');
+            assert.equal(savedObj3.name, 'Cloudbreak');
+        });
+    });
+
+    describe('get', () => {
+
+        var objectId = savedObj3._id;
+        var result = store.get(objectId);
+
+        it('retrieves the correct object based on provided id', () => {
+            assert.equal(result, savedObj3);
+
         });
     });
 });
+
+
