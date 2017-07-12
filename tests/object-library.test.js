97e40ba9ddf69bc7cef3838044f1ba4992030fcc
Wendy Beck <webeck@gmail.com>
2017-07-11 18:10:30 -0700

    forgot init commit. first function & test written

---
+++ tests/object-library.test.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+const {nightOut} = require ('../lib/object-library');
+
+describe('object library', () => {
+  it('creates an _id for an obj and returns it', () => {
+      let monday = new nightOut();
+      monday.save(monday);
+      return new Promise(function (resolve) {
+        // added a Promise because function was timing out
+        // it turned out to be because i hadn't required assert
+        // console.log('inside the promise');
+        resolve(assert.ok(monday._id));
+      })
+    });
+  });
