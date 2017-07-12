c1ca8ec0ae23bc04cfd528555ad2ef6f9e98ce38
Joe Churchman <churchman.joe@gmail.com>
2017-07-12 10:32:02 -0700

    update .gitignore, move store from test file to own file

--- test/object-store.test.js
+++ test/object-store.test.js
@@ -1,29 +1,5 @@
 const assert = require('assert');
-const shortid = require('shortid');
-
-shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
-
-const store = {
-  items: [],
-  getAll: () => store.items.slice(),
-  save: (obj) => {
-    obj._id = shortid.generate();
-    store.items.push(obj);
-    return obj;
-  },
-  get: (id) => {
-    if (store.items.find((element) => {
-      return element._id === id;
-    }, id)) {
-      return store.items.find((element) => {
-        return element._id === id;
-      }, id);
-    } else {
-      return null
-    }
-  }
-};
-
+const store = require('../lib/object-store');
 
 describe('object-store', () => {
   it('works', ()=> {
